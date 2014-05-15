f2c18b7263a74f40cadb5784fa987fc430af7a8b
Harold Short <haroldfredshort@gmail.com>
2014-05-15 03:05:18 -0700

    content added

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var http = require('http');
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Already Coding\n');
-}).listen(port);
+var fs = require('fs');
+fs.readFile('index.html', function(err, html) {
+	if (err) {
+		throw err;
+	}
+	http.createServer(function(req, res) {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.write(html);
+		res.end();
+	}).listen(port);
+});
