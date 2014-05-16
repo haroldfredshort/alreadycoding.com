468beef9b3d9e8a91e90e0a01b4b1d283dd9af85
Harold Short <hshort@epicor.com>
2014-05-16 01:38:39 -0700

    updated

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.all(/.*/, function(req, res, next) {
 	var host = req.header("host");
 	if (host.match(/^www\..*/i)) {
 		res.redirect(301, req.protocol + '://alreadycoding.com');
+		res.end();
 	} else {
 		next();
 	}
