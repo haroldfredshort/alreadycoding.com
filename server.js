4cfe2f2a9feaa5fe33859822f6e0b85d50875e01
Harold Short <hshort@epicor.com>
2014-05-15 23:37:07 -0700

    301 redirects for non-canonical URLs

--- server.js
+++ server.js
@@ -5,7 +5,16 @@ fs.readFile('index.html', function(err, html) {
 	if (err) {
 		throw err;
 	}
+	redirect = function(host, res, pattern, redirect){
+		if (host == pattern || (pattern instanceof RegExp && host.match(pattern))) {
+			res.writeHead(301, {
+			'location': redirect
+		});
+		res.end();
+	}};
 	http.createServer(function(req, res) {
+		redirect(req.headers.host, res, /^www/, req.protocol + '://alreadycoding.com');
+		redirect(req.headers.host, res, 'www.alreadycoding.com', req.protocol + '://alreadycoding.com');
 		res.writeHead(200, { 'Content-Type': 'text/html' });
 		res.write(html);
 		res.end();
