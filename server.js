5756708fb957d7abf9de089565f6579354c4f714
Harold Short <hshort@epicor.com>
2014-05-16 01:01:55 -0700

    moved to express

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-var http = require('http');
-var port = process.env.PORT || 1337;
-var fs = require('fs');
-fs.readFile('index.html', function(err, html) {
-	if (err) {
-		throw err;
+var express = require('express');
+var compress = require('compression');
+
+var app = express();
+
+app.use(compress());
+
+app.all(/.*/, function(req, res, next) {
+	var host = req.header("host");
+	if (host.match(/^www\..*/i)) {
+		res.redirect(301, req.protocol + '://alreadycoding.com');
+	} else {
+		next();
 	}
-	redirect = function(host, res, pattern, redirect){
-		if (host == pattern || (pattern instanceof RegExp && host.match(pattern))) {
-			res.writeHead(301, {
-			'location': redirect
-		});
-		res.end();
-	}};
-	http.createServer(function(req, res) {
-		redirect(req.headers.host, res, /^www/, req.protocol + '://alreadycoding.com');
-		redirect(req.headers.host, res, 'www.alreadycoding.com', req.protocol + '://alreadycoding.com');
-		res.writeHead(200, { 'Content-Type': 'text/html' });
-		res.write(html);
-		res.end();
-	}).listen(port);
 });
+
+app.use(express.static(__dirname + '/public'));
+
+var port = process.env.PORT || 1337;
+app.listen(port);
