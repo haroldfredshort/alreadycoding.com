1bb17602899f98cb4e6ae180677c9e7e35774d18
Harold Short <haroldfredshort@gmail.com>
2014-05-23 11:35:12 -0700

    Using angular directives and controllers

--- public/js/modules/SidebarModule.js
+++ public/js/modules/SidebarModule.js
@@ -1,29 +1,32 @@
-﻿/* globals angular */
+﻿var ngApp = angular.module("ngApp", []);
 
-(function () {
-    'use strict';
-
-    // Controller name is handy for logging
-    var controllerId = 'SidebarModule';
-
-    // Define the controller on the module.
-    // Inject the dependencies. 
-    // Point to the controller definition function.
-    angular.module('app').controller(controllerId, ['$scope', SidebarModule]);
-
-    function SidebarModule($scope) {
-        // Using 'Controller As' syntax, so we assign this to the vm variable (for viewmodel).
-        var vm = this;
-
-        // Bindable properties and functions are placed on vm.
-        vm.activate = activate;
-        vm.title = 'SidebarModule';
-
-        function activate() {
-        }
-
-        //#region Internal Methods        
-
-        //#endregion
+ngApp.directive("sidebarModule", function () {
+    return {
+        restrict: "E",
+        replace: true,
+        scope: function(){
+        },
+        template: "<div class='sidebar-module'>" +
+                    "<h4>{{title}}</h4>" +
+                    "<ol class='list-unstyled'>" +
+                      "<li ng-repeat='item in items'><a href='{{item.url}}'>{{item.value}}</a></li>" +
+                    "</ol>" +
+                  "</div>"
     }
-})();
+});
+
+ngApp.controller("SidebarHelloController", function ($scope) {
+    $scope.title = "Hello";
+    $scope.items = [
+        { value: "One" },
+        { value: "Two" }
+    ];
+});
+
+ngApp.controller("SidebarElsewhereController", function ($scope) {
+    $scope.title = "Elsewhere";
+    $scope.items = [
+        { value: "LinkedIn" },
+        { value: "Twitter" }
+    ];
+});
